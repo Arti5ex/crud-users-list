8851ccc19d6d09e974ed73c8e06ecdbc202fa42d
Artifex <artifex_90@mail.ru>
2017-12-25 00:19:03 +0800

    kiss

--- src/index.js
+++ src/index.js
@@ -8,28 +8,11 @@ import store from './store';
 import Main from './components/Main';
 import './index.css';
 
-// import PokemonPage from './components/PokemonPage';
-
-
-// render(
-//   <Provider store={store}>
-//     <Router>
-//       <div>  
-//         {/* <Route exact path="/" component={Main}/> */}
-//         {/* <Route path="/pokemon/:id" component={PokemonPage}/> */}
-//       </div>  
-//     </Router>
-//   </Provider>, 
-//   document.getElementById('app')
-// );
-
-
 render(
   <Provider store={store}>
     <Router>
       <div>  
         <Route exact path="/" component={Main}/>
-        {/* <Route path="/pokemon/:id" component={PokemonPage}/> */}
       </div>  
     </Router>
   </Provider>, 
